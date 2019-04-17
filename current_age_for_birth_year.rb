4c56f1b8490470712a6e86c2d443cd29ed643419
NaKia Whitby <whitby.nakia@gmail.com>
2019-04-17 18:20:30 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 def current_age_for_birth_year(birth_year)
   2003 - birth_year
 end
 
 current_age_for_birth_year(1984)
+=======
+def current_age_for_birth_year
+
+end
+>>>>>>> 52f70052a825c2b97da3ade5dee4f6a4e044f2e2
